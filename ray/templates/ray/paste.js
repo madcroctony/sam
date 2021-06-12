f91bb785d1a451ffc086f8913372299fb165de18
madcroctony <maranello.rs90723@outlook.jp>
2021-06-12 17:19:21 +0900

    first commit

--- ray/templates/ray/paste.js
+++ ray/templates/ray/paste.js
@@ -1,16 +1,10 @@
-// フレームをreloadする方法
-function doReloadTheFrame() {
-
-    // フレームのDOM要素を取得
-    var iframe = document.getElementById('paste_window');
-
-    // フレームをreload
-    iframe.contentWindow.location.reload(true);
-
+function doReloadTheFrame()
+{
+    var frame = document.getElementById('paste_window');
+    frame.contentWindow.location.reload(true);
 }
-window.addEventListener('load', function () {
 
-    // 2.5秒ごとに、フレームをreload
+window.addEventListener('load', function ()
+{
     setInterval(doReloadTheFrame, 2500);
-
 });
